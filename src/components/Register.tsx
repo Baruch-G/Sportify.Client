f74bb56fd084e7769265b947c1726e0da76ecd94
Baruch Ginzburg <W050459990@gmail.com>
2025-03-31 00:23:24 +0300

    build issue fixed

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -3,20 +3,15 @@ import {
     Stepper,
     Step,
     StepLabel,
+    StepIconProps, // Import StepIconProps
 } from '@mui/material';
-import SignIn from './SignIn';
 import { Check } from '@mui/icons-material';
+import SignIn from './SignIn';
 import SportsPreferencesSetup from './SportsPreferencesSetup';
 
 const steps = ['Sign In', 'Customization'];
 
-interface CustomIconProps {
-    active: boolean;
-    completed: boolean;
-    icon: React.ReactNode;
-}
-
-const CustomIcon: React.FC<CustomIconProps> = ({ active, completed, icon }) => {
+const CustomIcon: React.FC<StepIconProps> = ({ active, completed, icon, error }) => { // Change to StepIconProps
     const contents = completed ? <Check fontSize="inherit" /> : icon;
     return (
         <div
@@ -43,7 +38,7 @@ function StepperSignIn() {
 
     return (
         <div>
-            <div style={{height: "700px"}}>
+            <div style={{ height: "700px" }}>
                 {activeStep === 0 && <SignIn onSubmit={() => setActiveStep(1)} />}
                 {activeStep === 1 && <SportsPreferencesSetup onSubmit={() => setActiveStep(1)} />}
             </div>
