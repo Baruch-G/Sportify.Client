298a2d7ac15571763ccd400448376cae0aa276ea
Baruch Ginzburg <W050459990@gmail.com>
2025-03-30 17:22:41 +0300

    event list added on map page

--- src/map/MapBase.tsx
+++ src/map/MapBase.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Map from 'react-map-gl/maplibre';
+import { useState, useEffect, useRef } from 'react';
+import { Map, MapRef } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import mapboxgl, { Padding } from 'maplibre-gl';
-import maplibregl from "maplibre-gl";
+import mapboxgl, { MapLayerMouseEvent } from 'maplibre-gl';
 import EntityLoader from './EntityLoader';
 import EventCard, { Event } from "../components/EventCard";
-import "./MapBase.css"
+import "./MapBase.css";
 
 const mapTilerKey = import.meta.env.VITE_MAPTILER_KEY;
+const serverURL = import.meta.env.VITE_SPORTIFY_SERVER_URL;
 
 mapboxgl.setRTLTextPlugin(
     'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
@@ -17,32 +17,48 @@ mapboxgl.setRTLTextPlugin(
 
 function MapBase() {
     const [userPosition, setUserPosition] = useState<{ latitude: number; longitude: number } | null>(null);
-    const [eventEntities, setEventEntities] = useState<Event[]>([])
+    const [eventEntities, setEventEntities] = useState<Event[]>([]);
+    const [selectedEvent, setSelectedEvent] = useState<string>();
+
+    const mapRef = useRef<MapRef | null>(null);
+    const eventRefs = useRef<(HTMLDivElement | null)[]>([]); // Array of refs
+
+    const setSelectFeature = (e: MapLayerMouseEvent) => {
+        const map = mapRef.current?.getMap();
+        const features = map?.queryRenderedFeatures(e.point, {
+            layers: ["event-locations"],
+        });
+
+        if (features?.length && features[0]?.properties.id) {
+            setSelectedEvent(features[0]?.properties.id);
+
+            // Scroll to the selected event
+            const index = eventEntities.findIndex(event => event.id === features[0]?.properties.id || event._id === features[0]?.properties.id);
+            eventRefs.current[index]?.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-
-                const response = await fetch('https://sportify-qa-server.onrender.com/events');
+                const response = await fetch(`${serverURL}/events`);
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
                 const data = await response.json() as Event[];
                 setEventEntities(data);
             } catch (err: any) {
+                console.error(err);
             }
         };
         fetchData();
-    }, [])
+    }, []);
 
-    // Get user location
     useEffect(() => {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     const { latitude, longitude } = position.coords;
-                    console.log(({ latitude, longitude }));
-
                     setUserPosition({ latitude, longitude });
                 },
                 (error) => {
@@ -60,23 +76,28 @@ function MapBase() {
     }, []);
 
     return (
-        <div>
-            <div id="map-side-bar">
-                    {eventEntities.map(i => (<>
-                        <div style={{ padding: 30 }}>
-                            <EventCard {...i} />
-
-                        </div>
-                    </>))}
+        <div style={{ display: "flex" }}>
+            <div id="map-side-bar" style={{ overflowY: "auto", height: "calc(100vh - 55px)" }}>
+                {eventEntities.map((event, index) => (
+                    <div
+                        key={event.id}
+                        ref={(el) => (eventRefs.current[index] = el)}
+                        style={{ padding: 30 }}
+                    >
+                        <EventCard selected={selectedEvent} event={event} />
+                    </div>
+                ))}
             </div>
             <Map
+                id='EventMap'
+                ref={mapRef}
                 initialViewState={{
                     longitude: userPosition?.longitude || 34.77262,
                     latitude: userPosition?.latitude || 31.99047,
                     zoom: 12
                 }}
+                onClick={setSelectFeature}
                 localIdeographFontFamily="'Noto Sans', 'Noto Sans CJK SC', sans-serif"
-                mapLib={maplibregl}
                 style={{
                     position: "absolute",
                     top: "55px",
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Grid, CircularProgress, Typography } from '@mui/material';
 import EventCard, { Event } from '../components/EventCard';
 
-function RunningCardContainer() {
+const serverURL = import.meta.env.VITE_SPORTIFY_SERVER_URL;
+
+function EventList() {
   const [events, setEvents] = useState<Event[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -10,15 +12,14 @@ function RunningCardContainer() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        
-        const response = await fetch('https://sportify-qa-server.onrender.com/events');
+        const response = await fetch(`${serverURL}/events`);
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
         const data = await response.json() as Event[];
         setEvents(data);
         setLoading(false);
-      } catch (err : any) {
+      } catch (err: any) {
         setError(err);
         setLoading(false);
       }
@@ -48,11 +49,11 @@ function RunningCardContainer() {
     <Grid container spacing={2} justifyContent="center" style={{ padding: '20px' }}>
       {events.map((event, index) => (
         <Grid item xs={12} sm={6} md={4} key={index}>
-          <EventCard {...event} />
+          <EventCard event={event} />
         </Grid>
       ))}
     </Grid>
   );
 }
 
-export default RunningCardContainer;
+export default EventList;
