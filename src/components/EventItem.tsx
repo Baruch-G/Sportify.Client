419c73efd86d2a54545e3e0a0186813b4b8df6be
Baruch Ginzburg <W050459990@gmail.com>
2025-05-10 22:27:46 +0300

    color issue fixed

--- src/components/EventItem.tsx
+++ src/components/EventItem.tsx
@@ -37,89 +37,88 @@ const EventItem = (props: EventItemProps) => {
             boxShadow: '0 2px 4px rgba(0,0,0,0.05)',
             transition: 'all 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
             '&:hover': {
-            boxShadow: '0 8px 16px rgba(0,0,0,0.1)',
-            transform: 'translateY(-4px)',
-            borderColor: 'grey',
+                boxShadow: '0 4px 8px rgba(227, 113, 39, 0.15)',
+                transform: 'translateY(-4px)',
+                borderColor: 'grey',
             },
             '&:before': {
-            content: '""',
-            position: 'absolute',
-            left: 0,
-            top: 0,
-            bottom: 0,
-            width: '6px',
-            backgroundColor: props.event.category.color,
-            borderRadius: '4px 0 0 4px',
+                content: '""',
+                position: 'absolute',
+                left: 0,
+                top: 0,
+                bottom: 0,
+                width: '6px',
+                borderRadius: '4px 0 0 4px'
             }
         }}>
             <CardContent sx={{
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 2.5,
-            p: 3
-            }}>
-            <Box sx={{ 
-                display: 'flex', 
-                alignItems: 'center',
-                gap: 1.5,
-                pb: 1,
-                borderBottom: '1px solid',
-                borderColor: 'divider'
-            }}>
-                <Box sx={{ 
-                color: 'primary.main',
+                height: '100%',
                 display: 'flex',
-                alignItems: 'center',
-                fontSize: '1.5rem'
+                flexDirection: 'column',
+                gap: 2.5,
+                p: 3
+            }}>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: 1.5,
+                    pb: 1,
+                    borderBottom: '1px solid',
+                    borderColor: 'divider'
                 }}>
-                {sportToIconMap[props.event.category.name.replace(' ', '-').toLowerCase() as keyof typeof sportToIconMap]}
+                    <Box sx={{
+                        color: 'primary.main',
+                        display: 'flex',
+                        alignItems: 'center',
+                        fontSize: '1.5rem'
+                    }}>
+                        {sportToIconMap[props.event.category.name.replace(' ', '-').toLowerCase() as keyof typeof sportToIconMap]}
+                    </Box>
+                    <Typography variant="h5" component="div" noWrap sx={{
+                        fontWeight: 600,
+                        color: 'text.primary'
+                    }}>
+                        {props.event.category.name}
+                    </Typography>
                 </Box>
-                <Typography variant="h5" component="div" noWrap sx={{ 
-                fontWeight: 600,
-                color: 'text.primary'
-                }}>
-                {props.event.category.name}
-                </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
-                <CalendarTodayIcon sx={{ 
-                color: 'success.main',
-                flexShrink: 0,
-                fontSize: '1.25rem'
-                }} />
-                <Typography variant="body1" noWrap sx={{ color: 'text.secondary' }}>
-                {new Date(props.event.date).toLocaleDateString(undefined, {
-                    weekday: 'long',
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric'
-                })}
-                </Typography>
-            </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+                    <CalendarTodayIcon sx={{
+                        color: 'success.main',
+                        flexShrink: 0,
+                        fontSize: '1.25rem'
+                    }} />
+                    <Typography variant="body1" noWrap sx={{ color: 'text.secondary' }}>
+                        {new Date(props.event.date).toLocaleDateString(undefined, {
+                            weekday: 'long',
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric'
+                        })}
+                    </Typography>
+                </Box>
 
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
-                <LocationOnIcon sx={{ 
-                color: 'error.main',
-                flexShrink: 0,
-                fontSize: '1.25rem'
-                }} />
-                <Typography variant="body1" noWrap sx={{ color: 'text.secondary' }}>
-                {props.event.address.addressLine1}
-                {props.event.address.addressLine2 && `, ${props.event.address.addressLine2}`}
-                {`, ${props.event.address.city}`}
-                </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
-                <AccessTimeIcon sx={{ 
-                color: 'error.main',
-                flexShrink: 0,
-                fontSize: '1.25rem'
-                }} />
-                <Typography variant="body1" noWrap sx={{ color: 'text.secondary' }}>
-                {new Date(props.event.date).toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', hour12: false })} | {props.event.duration}h
-                </Typography>
-            </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+                    <LocationOnIcon sx={{
+                        color: 'error.main',
+                        flexShrink: 0,
+                        fontSize: '1.25rem'
+                    }} />
+                    <Typography variant="body1" noWrap sx={{ color: 'text.secondary' }}>
+                        {props.event.address.addressLine1}
+                        {props.event.address.addressLine2 && `, ${props.event.address.addressLine2}`}
+                        {`, ${props.event.address.city}`}
+                    </Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1.5 }}>
+                    <AccessTimeIcon sx={{
+                        color: 'error.main',
+                        flexShrink: 0,
+                        fontSize: '1.25rem'
+                    }} />
+                    <Typography variant="body1" noWrap sx={{ color: 'text.secondary' }}>
+                        {new Date(props.event.date).toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', hour12: false })} | {props.event.duration}h
+                    </Typography>
+                </Box>
             </CardContent>
         </Card>
     )
